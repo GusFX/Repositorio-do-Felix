a438e8cb54c6590960f56e635243e03147412fbd
GusFX <37117188+GusFX@users.noreply.github.com>
2018-03-17 18:06:26 -0300

    Update t1.hs

--- t1/t1.hs
+++ t1/t1.hs
@@ -1,3 +1,5 @@
+import Data.Char
+
 -- Eleva um numero ao quadrado (aqui temos um comentario!)
 doubleMe :: Int -> Int
 doubleMe x = x^2
@@ -25,72 +27,139 @@ doubleFirst lis = (head lis)^2
 isLong :: String -> Bool -- isso é o mesmo que: isLong :: [Char] -> Bool
 isLong s = length s > 2
 
+
+--Prática 1
+
+--1
 somaQuad :: Int -> Int -> Int
 somaQuad x y = (x^2) + (y^2)
 
+--2
 hasEqHeads :: [Int] -> [Int] -> Bool
 hasEqHeads x y = if (head x) == (head y) then True else False
 
---irá subtrair a idade do ano atu
-
-cat :: String -> String
-cat x = "<LI>" ++ x ++ "</LI>"
-
+--3
 catSuper :: [String] -> [String]
-catSuper x = map (cat) x
+catSuper x = map ("Super "++) x
 
+--4
 contSpace :: String -> String
 contSpace x = filter(== ' ') x
 
 contStr :: String -> Int
 contStr x = length(contSpace x)
 
+--5
 calAnonima :: [Float] -> [Float]
 calAnonima x = map(\n->3*n^2 + 2/n + 1) x
 
+--6
 selectNeg :: [Int] -> [Int]
 selectNeg x = filter(<0) x
 
+--7
 enTre :: [Int] -> [Int]
 enTre list = filter(\n-> (n>0) && (n<101)) list
 
+--8
 idadeVelho :: [Int] -> [Int]
 idadeVelho list = filter(\n-> (2018-n)>1980) list
- 
 
---Outro dia, outros exercícios
+--9
+areEven :: [Int] -> [Int]
+areEven list = filter(isEven) list 
 
+--10
+charFound :: Char -> String -> Bool
+charFound a list = a `elem` list
+
+--11
+checkLast :: String -> Bool
+checkLast c = if (last c) == 'a' then True else False
+
+lastA :: [String] -> [String]
+lastA list = filter (checkLast) list
+
+
+--Prática 2
+
+--1
 isVowels :: Char -> Bool
-isVowels x | x == 'a' = True | x == 'e' = True | x == 'i' = True | x == 'o' = True | x == 'u' = True | otherwise = False
+isVowels c = c `elem` "aeiouAEIOU"
 
-isnotVowels :: Char -> Bool
-isnotVowels x | x == 'a' = False | x == 'e' = False | x == 'i' = False | x == 'o' = False | x == 'u' = False | otherwise = True 
-
-cat2 :: String -> String
-cat2 x = x ++ "<LI>"
-
+--2
 addComma :: [String] -> [String]
-addComma list = map (cat2) list	
+addComma list = map (++ ",") list
 
+--3
 htmlListItems :: [String] -> [String]
-htmlListItems list = map(cat) list
+htmlListItems list = map(\n-> "<LI>" ++ n ++ "</LI>") list
+
+htmlCat :: String -> String
+htmlCat x = "<LI>" ++ x ++ "</LI>"
 
 htmlListItems2 :: [String] -> [String]
-htmlListItems2 list = map(\n-> "<LI>" ++ n ++ "</LI>") list
+htmlListItems2 list = map(htmlCat) list
 
+--4
 semVogais :: String -> String
-semVogais pal = filter(isnotVowels) pal
+semVogais pal = filter (\c -> c `notElem` "aeiouAEIOU") pal
+
+semVogais2 :: String -> String
+semVogais2 pal = filter(not.isVowels) pal
+
+--5
+trocaLetra :: String -> String
+trocaLetra t = map(\x->if x /= ' ' then '-' else ' ') t
 
 isnotSpace :: Char -> Char
 isnotSpace x = if x /= ' ' then '-' else ' '
 
-trocaLetra :: String -> String
-trocaLetra t = map(isnotSpace) t
-
 trocaLetra2 :: String -> String
-trocaLetra2 t = map(\x->if x /= ' ' then '-' else ' ') t 
+trocaLetra2 t = map(isnotSpace) t
 
+--6
 firstName :: String -> String
 firstName n = takeWhile (/= ' ') n
 
+--7
+naoehNum :: Char -> Bool
+naoehNum c = c `notElem` "0123456789"
+
+isInt :: String -> Bool
+isInt list = if length(filter (naoehNum) list) >0 then False else True
+
+--8
+lastName :: String -> String
+lastName n = last(words n)
+
+--9
+userName :: String -> String
+userName n = map (toLower)(take 1 n) ++ map (toLower)(lastName n)
+
+--10
+mapaLetras :: Char -> Char
+mapaLetras x | x == 'a' = '4' | x == 'e' = '3' | x == 'i' = '2' | x == 'o' = '1' | x == 'u' = '0' | otherwise = x
+
+encodeName :: String -> String
+encodeName n = map (mapaLetras) n
+
+--11
+--Incompleto!
+betterMapaLetras :: Char -> Char
+betterMapaLetras x | x == 'a' = '4' | x == 'e' = '3' | x == 'i' = '1' | x == 'o' = '0' | otherwise = x
+
+betterEncodeName :: String -> String
+betterEncodeName n = map(betterMapaLetras) n 
+
+--12
+--Incompleto!
+completaPal :: String -> String
+completaPal x = if length(x) <10 then completaPal x++"."
+
+auxPal :: String -> String
+auxPal x = if length(x) >10 then (take 10 x) else completaPal x
+
+stringDez :: [String] -> [String]
+stringDez list = map(encodeName) list
 
