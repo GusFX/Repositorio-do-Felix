17c6ad1b107776b55554b6549fcde959dffcf709
GusFX <37117188+GusFX@users.noreply.github.com>
2018-04-02 08:56:29 -0300

    Update t2parte3.hs

--- t2/t2parte3.hs
+++ t2/t2parte3.hs
@@ -1,1 +1,13 @@
+import Data.Char
 
+isEanOk :: String -> Bool
+isEanOk str = if (length str) == 13 then digitToInt(last str) == checaUltimo str else False
+
+checaUltimo :: String -> Int
+checaUltimo num  = 10 - (mod (somaTudo num) 10)	
+
+somaTudo :: String -> Int
+somaTudo num = sum (zipWith (*) (string2intlist num) [1,3,1,3,1,3,1,3,1,3,1,3])
+
+string2intlist :: String -> [Int]
+string2intlist str = map (digitToInt) str
